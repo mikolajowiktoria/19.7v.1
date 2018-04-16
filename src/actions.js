30ce3aa1c1130edbf4f120aa61c905df9bbbcc05
Wiktoria Mikołajów <mikolajow.w@gmail.com>
2018-04-16 19:09:13 +0200

    finished task

---
+++ src/actions.js
@@ -0,0 +1,45 @@
+import uuid from 'uuid';
+
+
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+export function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4()
+    }
+}
+
+export function editComment(text, commentId) {
+    return {
+        type: EDIT_COMMENT,
+        text,
+        id: commentId
+    }
+}
+
+export function removeComment(commentId) {
+    return {
+        type: REMOVE_COMMENT,
+        id: commentId
+    }
+}
+
+export function thumbUpComment(commentId) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id: commentId
+    }
+}
+
+export function thumbDownComment(commentId) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id: commentId
+    }
+}
